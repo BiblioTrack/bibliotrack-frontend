c095f337843e8f68993557f5b311db0b8f8fb3f6
Aya Saafan <ayasafan16@gmail.com>
2023-11-17 22:38:55 +0100

    create TopNavbar component

---
+++ src/components/TopNavbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+
+function TopNavbar() {
+  return (
+    <Navbar className="navbar navbar-dark bg-dark">
+      <Navbar.Brand href="#home">BiblioTrack</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        {/* Your navigation links go here */}
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default TopNavbar;
