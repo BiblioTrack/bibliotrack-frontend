9fb08e80c620131b462bac31c4359d8f4d7a02ee
Aya Saafan <ayasafan16@gmail.com>
2023-11-25 13:27:55 +0100

    Create VerticalSpace Component

---
+++ src/components/VerticalSpace.js
@@ -0,0 +1,8 @@
+// VerticalSpace.js
+import React from 'react';
+
+const VerticalSpace = ({ margin }) => {
+  return <div style={{ marginTop: `${margin}px` }}></div>;
+};
+
+export default VerticalSpace;
