651ed83594802ea6c583a3a50a26703e1dcfe039
Aya Saafan <ayasafan16@gmail.com>
2023-11-28 16:33:25 +0100

    Add BookCard

---
+++ src/components/BookCard.js
@@ -0,0 +1,18 @@
+// BookCard.js
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import './BookCard.css'
+
+const BookCard = ({ book, onBookClick }) => {
+  return (
+    <Card className="book-item" onClick={() => onBookClick(book.id)}>
+      <Card.Img variant="top" src={book.coverImage} alt={book.title} />
+      <Card.Body>
+        <Card.Title className="book-title">{book.title}</Card.Title>
+        <Card.Text>by {book.author}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default BookCard;
