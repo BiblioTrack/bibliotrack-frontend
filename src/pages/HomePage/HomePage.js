4960f962c3b47fdb010a0c60ce5110a220780f84
Aya Saafan <ayasafan16@gmail.com>
2023-11-28 16:34:10 +0100

    Add Search Results to Home

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,12 +1,15 @@
 import TopNavbar from '../../components/TopNavbar.js';
 import SearchSection from './components/SearchSection.js';
 import VerticalSpace from '../../components/VerticalSpace.js';
+import SearchResultsSection from './components/SearchResultsSection.js';
 
 function Home() {
     return <>
       <TopNavbar/>
       <VerticalSpace margin={30} /> 
       <SearchSection/>
+      <VerticalSpace margin={20} /> 
+      <SearchResultsSection/>
     </>;
   }
   
