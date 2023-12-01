55324cd3adc32ad7ac52ce18561e70b973a55530
Aya Saafan <ayasafan16@gmail.com>
2023-12-01 19:18:54 +0100

    Add BookDetailsPage.js

---
+++ src/pages/BookDetailsPage/BookDetailsPage.js
@@ -0,0 +1,27 @@
+// BookDetailsPage.js
+import React from 'react';
+
+import TopNavbar from '../../components/TopNavbar.js';
+import VerticalSpace from '../../components/VerticalSpace.js';
+
+import { useParams } from 'react-router-dom';
+import BookDetails from './components/BookDetails.js';
+import booksData from '../../assets/booksData.json';
+
+const BookDetailsPage = () => {
+
+  const { id } = useParams();
+  
+  //TODO: Use API GET book endpoint
+  const selectedBook = booksData.find((book) => book.id.toString() === id);
+
+  return (
+    <>
+      <TopNavbar/>
+      <VerticalSpace margin={30} />     
+      {selectedBook ? <BookDetails book={selectedBook} /> : <p>Book not found</p>}
+    </>
+  );
+};
+
+export default BookDetailsPage;
