cad99f711b6c773cb5ee09b5d8a0f7bb6944f119
Aya Saafan <ayasafan16@gmail.com>
2023-12-02 12:43:40 +0100

    Fix pages routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/HomePage/HomePage.js'
 import About from './pages/AboutPage/AboutPage.js'
+import Login from './pages/LoginPage/LoginPage.js'
+import Signup from './pages/SignupPage/SignupPage.js';
+import BookDetailsPage from './pages/BookDetailsPage/BookDetailsPage.js'
 
 
 function App() {
@@ -11,6 +14,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/about" element={<About/>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/book/:id" element={<BookDetailsPage />} />
       </Routes>
     </BrowserRouter>
 
