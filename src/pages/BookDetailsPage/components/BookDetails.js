c78f00019133e9fab413f231765d5085b3aacdb2
Aya Saafan <ayasafan16@gmail.com>
2023-12-02 14:39:52 +0100

    use bootstrap standard margin

--- src/pages/BookDetailsPage/components/BookDetails.js
+++ src/pages/BookDetailsPage/components/BookDetails.js
@@ -11,23 +11,23 @@ const BookDetails = ({ book }) => {
          {/* Image and Request Borrowing Button */}
          <Col lg={3} md={4} className="book-details-image text-center">
           <Image src={book.coverImage} alt={book.title} fluid className="bookcover-image" />
-          <Button variant="outline-dark" className="request-button w-100">
+          <Button variant="outline-dark" className="request-button mt-3">
             Request Copy
           </Button>
         </Col>
 
         {/* Book Details */}
         <Col lg={9} md={8} className="book-details-info mt-5 mt-md-0">
-          <div className="unique-title">{book.title}</div>
-          <div className="unique-author">{book.author}</div>
-          <div className="genre-badges">
+          <div className="unique-title mb-2">{book.title}</div>
+          <div className="unique-author mb-2">{book.author}</div>
+          <div className="mb-2">
             {book.genres.map((genre, index) => (
-              <Badge key={index} pill bg="light" className="genre-badge">
+              <Badge key={index} pill bg="light" className="genre-badge mr-1">
                 {genre}
               </Badge>
             ))}
           </div>
-          <p className="description">{book.description}</p>
+          <p className="mb-3">{book.description}</p>
           <p><strong>Pages:</strong> {book.pages}</p>
           <p><strong>Format:</strong> {book.format}</p>
           <p><strong>Available Copies:</strong> {book.availableCopies}</p>
