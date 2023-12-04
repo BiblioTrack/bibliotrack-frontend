908229e976c53929ccd980f1fe0dace09ab9516e
sapanajain9826 <sapanajain9826@gmail.com>
2023-12-04 17:34:11 +0100

    Add a admin page for adding a book

--- src/App.js
+++ src/App.js
@@ -6,16 +6,18 @@ import About from './pages/AboutPage/AboutPage.js'
 import Login from './pages/AuthPages/LoginPage/LoginPage.js'
 import Signup from './pages/AuthPages/SignupPage/SignupPage.js';
 import BookDetailsPage from './pages/BookDetailsPage/BookDetailsPage.js'
+import AddBook from './pages/AdminPage/AddBook.js';
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/about" element={<About/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/addbook" element={<AddBook />} />
         <Route path="/book/:id" element={<BookDetailsPage />} />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/AdminPage/components/AddBookSection.js
@@ -0,0 +1,247 @@
+import React, { useState } from 'react';
+import { Form, FormControl, Row, Col, Button } from 'react-bootstrap';
+// import { Link } from 'react-router-dom';
+import AdminSection from './AdminSection';
+
+
+const AddBookSection = () => {
+
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const [isbn, setIsbn] = useState('');
+  const [category, setCategory] = useState('');
+
+  const [edition, setEdition] = useState('');
+  const [publishYear, setPublishYear] = useState('');
+
+  const [editor, setEditor] = useState('');
+  const [publisher, setPublisher] = useState('');
+
+
+  const [copies, setCopies] = useState('');
+  const [pageCount, setPageCount] = useState('');
+
+  const [shelf, setShelf] = useState('');
+  const [floor, setFloor] = useState('');
+
+  const [createdAt, setCreatedAt] = useState('');
+  const [updatedAt, setUpdatedAt] = useState('');
+
+  const [description, setDescription] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add new book addition logic here
+  };
+
+  return (
+
+    <AdminSection title="Add New Book to the System" description="">
+      <Form className="mt-5" onSubmit={handleSubmit} title="Add New Book to the System" align="left">
+
+        <Row>
+          <Col md={6} className="mb-3">
+            <Form.Group className="mb-3" controlId="title">
+              <Form.Label>Title</Form.Label>
+              <FormControl
+                type="text"
+                placeholder="Title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </Form.Group>
+          </Col>
+          <Col md={6} className="mb-3">
+            <Form.Group className="mb-3" controlId="author">
+              <Form.Label>Author</Form.Label>
+              <FormControl
+                type="text"
+                placeholder="Author"
+                value={author}
+                onChange={(e) => setAuthor(e.target.value)}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col md={6} className="mb-3">
+            <Form.Group className="mb-3" controlId="isbn">
+              <Form.Label>ISBN</Form.Label>
+              <FormControl
+                type="text"
+                placeholder="ISBN"
+                value={isbn}
+                onChange={(e) => setIsbn(e.target.value)}
+              />
+            </Form.Group>
+          </Col>
+          <Col md={6} className="mb-3">
+            <Form.Group className="mb-3" controlId="category">
+              <Form.Label>Category</Form.Label>
+              <FormControl
+                type="text"
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col md={6} className="mb-3">
+            <Form.Group className="mb-3" controlId="edition">
+              <Form.Label>Edition</Form.Label>
+              <FormControl
+                type="int"
+                placeholder="Edition"
+                value={edition}
+                onChange={(e) => setEdition(e.target.value)}
+              />
+            </Form.Group>
+          </Col>
+          <Col md={6} className="mb-3">
+            <Form.Group className="mb-3" controlId="publishYear">
+              <Form.Label>Publish Year</Form.Label>
+              <FormControl
+                type="text"
+                value={publishYear}
+                onChange={(e) => setPublishYear(e.target.value)}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col md={6} className="mb-3">
+            <Form.Group className="mb-3" controlId="editor">
+              <Form.Label>Editor</Form.Label>
+              <FormControl
+                type="text"
+                placeholder="editor"
+                value={editor}
+                onChange={(e) => setEditor(e.target.value)}
+              />
+            </Form.Group>
+          </Col>
+          <Col md={6} className="mb-3">
+            <Form.Group className="mb-3" controlId="publisher">
+              <Form.Label>Publisher</Form.Label>
+              <FormControl
+                type="text"
+                value={publisher}
+                onChange={(e) => setPublisher(e.target.value)}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col md={6} className="mb-3">
+            <Form.Group className="mb-3" controlId="copies">
+              <Form.Label>Copies</Form.Label>
+              <FormControl
+                type="text"
+                placeholder="Copies"
+                value={copies}
+                onChange={(e) => setCopies(e.target.value)}
+              />
+            </Form.Group>
+          </Col>
+          <Col md={6} className="mb-3">
+            <Form.Group className="mb-3" controlId="pageCount">
+              <Form.Label>Page Count</Form.Label>
+              <FormControl
+                type="int"
+                placeholder="Page Count"
+                value={pageCount}
+                onChange={(e) => setPageCount(e.target.value)}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col md={6} className="mb-3">
+            <Form.Group className="mb-3" controlId="shelf">
+              <Form.Label>Shelf</Form.Label>
+              <FormControl
+                type="int"
+                placeholder="Shelf"
+                value={shelf}
+                onChange={(e) => setShelf(e.target.value)}
+              />
+            </Form.Group>
+          </Col>
+          <Col md={6} className="mb-3">
+            <Form.Group className="mb-3" controlId="floor">
+              <Form.Label>Floor</Form.Label>
+              <FormControl
+                type="int"
+                placeholder="Floor"
+                value={floor}
+                onChange={(e) => setFloor(e.target.value)}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col md={6} className="mb-3">
+            <Form.Group className="mb-3" controlId="createdAt">
+              <Form.Label>Created At</Form.Label>
+              <FormControl
+                type="date"
+                placeholder="Created At"
+                value={createdAt}
+                onChange={(e) => setCreatedAt(e.target.value)}
+              />
+            </Form.Group>
+          </Col>
+          <Col md={6} className="mb-3">
+            <Form.Group className="mb-3" controlId="updatedAt">
+              <Form.Label>Updated At</Form.Label>
+              <FormControl
+                type="date"
+                placeholder="Updated At"
+                value={updatedAt}
+                onChange={(e) => setUpdatedAt(e.target.value)}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+
+        <Form.Group className="mb-3" controlId="description">
+          <Form.Label>Description</Form.Label>
+          <FormControl
+            type="text"
+            placeholder="Description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </Form.Group>
+
+
+
+
+
+
+        <Button type="submit" variant="info" className="mb-3 admin-button"> Add </Button>
+
+        {/* <div className="additional-options mt-3">
+          <p>
+            Already have an account?{' '}
+            <Link to="/login" className="text-info">
+              Login here
+            </Link>
+          </p>
+        </div> */}
+
+      </Form>
+    </AdminSection >
+  );
+};
+
+export default AddBookSection;
+
