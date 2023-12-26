8255f1251ac5def8c3e8a5c70b71b4aaf809fa90
Aya Saafan <ayasafan16@gmail.com>
2023-12-26 01:11:39 +0100

    change date fields to type "date"

--- src/pages/BookDetailsPage/components/IssueCopyModal.js
+++ src/pages/BookDetailsPage/components/IssueCopyModal.js
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
 import { Modal, Button, Form } from 'react-bootstrap';
 
-const IssueCopyModal = ({ show, onHide , bookId}) => {
+const IssueCopyModal = ({ show, onHide, bookId }) => {
   const [copyNumber, setCopyNumber] = useState('');
   const [borrowerId, setBorrowerId] = useState('');
-  const [issueDate, setIssueDate] = useState('');
-  const [returnDate, setReturnDate] = useState('');
+  const [issueDate, setIssueDate] = useState(
+    new Date().toISOString().split('T')[0] 
+  );
+  const [returnDate, setReturnDate] = useState(
+    new Date(new Date()+7).toISOString().split('T')[0]
+  );
 
   const handleIssueCopy = () => {
-    // Perform any validation or additional logic as needed
-    // Then, call the onIssueCopy function to handle the issuance
+    // TODO: onIssueCopy API
     const issueData = {
       bookId,
       copyNumber,
@@ -18,7 +21,7 @@ const IssueCopyModal = ({ show, onHide , bookId}) => {
       returnDate,
     };
 
-    console.log(`Issuing copy : ${issueData}`);
+    console.log(`Issuing copy: ${JSON.stringify(issueData)}`);
 
     // Close the modal after issuing the copy
     onHide();
@@ -31,7 +34,7 @@ const IssueCopyModal = ({ show, onHide , bookId}) => {
       </Modal.Header>
       <Modal.Body>
         <Form>
-          <Form.Group controlId="copyNumber"  className="mb-3" >
+          <Form.Group controlId="copyNumber" className="mb-3">
             <Form.Label>Copy Number</Form.Label>
             <Form.Control
               type="text"
@@ -39,7 +42,7 @@ const IssueCopyModal = ({ show, onHide , bookId}) => {
               onChange={(e) => setCopyNumber(e.target.value)}
             />
           </Form.Group>
-          <Form.Group controlId="borrowerId"  className="mb-3" >
+          <Form.Group controlId="borrowerId" className="mb-3">
             <Form.Label>User ID</Form.Label>
             <Form.Control
               type="text"
@@ -47,18 +50,18 @@ const IssueCopyModal = ({ show, onHide , bookId}) => {
               onChange={(e) => setBorrowerId(e.target.value)}
             />
           </Form.Group>
-          <Form.Group controlId="issueDate"  className="mb-3" >
+          <Form.Group controlId="issueDate" className="mb-3">
             <Form.Label>Issue Date</Form.Label>
             <Form.Control
-              type="text"
+              type="date"
               value={issueDate}
               onChange={(e) => setIssueDate(e.target.value)}
             />
           </Form.Group>
-          <Form.Group controlId="returnDate"  className="mb-3" >
+          <Form.Group controlId="returnDate" className="mb-3">
             <Form.Label>Return Date</Form.Label>
             <Form.Control
-              type="text"
+              type="date"
               value={returnDate}
               onChange={(e) => setReturnDate(e.target.value)}
             />
