ef13867d6023af485d9539cd054ecfb6a95351ed
Aya Saafan <ayasafan16@gmail.com>
2023-12-26 02:09:31 +0100

    add IssueHistory  section

--- src/pages/BookPage/components/Book.js
+++ src/pages/BookPage/components/Book.js
@@ -4,12 +4,22 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import BookImageAndButtons from './BookImageAndButtons.js';
 import BookInfo from './BookInfo.js';
+import IssueHistory from './IssueHistory.js';
 import { useAuth } from '../../AuthPages/AuthContext.js';
 import './Book.css'; 
 
 
 const Book = ({ book }) => {
   const { isAdmin } = useAuth();
+
+  
+  // Dummy issue history for testing
+  const dummyIssueHistory = [
+    { userId: '123', issueDate: '2023-01-01', returnDate: '2023-01-15' },
+    { userId: '456', issueDate: '2023-02-01', returnDate: '2023-02-15' },
+    // Add more dummy data as needed
+  ];
+
 
   return (
     <Container className="book-details-container">
@@ -19,10 +29,17 @@ const Book = ({ book }) => {
           <BookImageAndButtons isAdmin={isAdmin} book={book} />
         </Col>
 
-        {/* Book Details */}
         <Col lg={9} md={8} className="book-details-info mt-5 mt-md-0">
+          {/* Book Info */}
           <BookInfo book={book} />
+
+          <hr className="my-5" />
+
           {/* Add more details as needed */}
+          {isAdmin &&
+            <IssueHistory issueHistory={dummyIssueHistory} />
+          }
+
         </Col>
       </Row>
     </Container>
---
+++ src/pages/BookPage/components/IssueHistory.js
@@ -0,0 +1,29 @@
+// IssueHistorySection.js
+import React from 'react';
+import { Table } from 'react-bootstrap';
+
+const IssueHistory = ({ issueHistory }) => (
+  <div className="mb-5">
+    <h4>Issue History</h4>
+    <Table striped bordered responsive>
+      <thead>
+        <tr>
+          <th>User ID</th>
+          <th>Issue Date</th>
+          <th>Return Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        {issueHistory.map((issue, index) => (
+          <tr key={index}>
+            <td>{issue.userId}</td>
+            <td>{issue.issueDate}</td>
+            <td>{issue.returnDate}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  </div>
+);
+
+export default IssueHistory;
