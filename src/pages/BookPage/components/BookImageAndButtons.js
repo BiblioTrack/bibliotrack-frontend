9a3690060855c418129752036e8604b35bdbf8be
Aya Saafan <ayasafan16@gmail.com>
2024-01-18 18:02:15 +0100

    add Book Delete

--- src/pages/BookPage/components/BookImageAndButtons.js
+++ src/pages/BookPage/components/BookImageAndButtons.js
@@ -3,9 +3,14 @@ import React, { useState } from 'react';
 import { Image, Button } from 'react-bootstrap';
 import IssueCopyModal from './IssueCopyModal.js';
 import RequestCopyModal from './RequestCopyModal.js';
+import ConfirmDeleteModal from './ConfirmDeleteModal';
+import { useNavigate } from 'react-router-dom';
+
 
 
 const BookImageAndButtons = ({ isAdmin , book }) => {
+  const navigate = useNavigate();
+
   /*For Admin Issue Copy*/
   const [showIssueCopyModal, setShowIssueCopyModal] = useState(false);
   const handleShowIssueCopyModal = () => {
@@ -22,6 +27,27 @@ const BookImageAndButtons = ({ isAdmin , book }) => {
   };
   const handleHideRequestCopyModal = () => {
     setRequestCopyModal(false);
+  };
+
+  /*For Delete Book*/
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+
+  const handleDeleteClick = () => {
+    setShowDeleteModal(true);
+  };
+
+  const handleConfirmDelete = () => {
+    // Perform the actual delete operation
+    console.log(book.id);
+    navigate('/');
+
+    // Close the modal
+    setShowDeleteModal(false);
+  };
+
+  const handleCancelDelete = () => {
+    // Close the modal without performing any action
+    setShowDeleteModal(false);
   };
 
   return (
@@ -45,14 +71,24 @@ const BookImageAndButtons = ({ isAdmin , book }) => {
             <Button variant="outline-dark" className="wide-button mt-3">
               Edit Book
             </Button>
+            <Button onClick={handleDeleteClick} variant="outline-dark" className="wide-button mt-3">
+              Delete Book
+            </Button>
               <Button variant="outline-dark" className="wide-button mt-3" onClick={handleShowIssueCopyModal}>
                 Issue Copy
               </Button>
+
                <IssueCopyModal
                 show={showIssueCopyModal}
                 onHide={handleHideIssueCopyModal}
                 bookId={book.id}
               />
+
+                <ConfirmDeleteModal
+                  show={showDeleteModal}
+                  onHide={handleCancelDelete}
+                  onConfirm={handleConfirmDelete}
+                />
                            
             </>
           }
