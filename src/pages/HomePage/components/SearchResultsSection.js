79c70fe46e2341875a2019418274c167d6043623
sapanajain9826 <sapanajain9826@gmail.com>
2024-01-18 07:25:55 +0530

    Added API call for get all books

--- src/pages/HomePage/components/SearchResultsSection.js
+++ src/pages/HomePage/components/SearchResultsSection.js
@@ -1,21 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import BookGrid from './BookGrid.js'; 
+import BookGrid from './BookGrid.js';
 import booksData from '../../../assets/booksData.json'; // Import the dummy data
-import './SearchResultsSection.css'; 
+import './SearchResultsSection.css';
+import { fetchAllBooks } from '../../../ApiCalls.js'
 
 const SearchResultsSection = ({ searchQuery }) => {
   const [books, setBooks] = useState([]);
 
   useEffect(() => {
     //TODO: Use API GET books endpoint
-    /*
-    const books= await fetchAllBook();
-    setBooks(books);
-    */
 
-    //Mock
-    setBooks(booksData);
+    fetchAllBooks()
+      .then(data => setBooks(data))
+      .catch(error => console.error('Error setting books:', error));
   }, []);
+
 
   // Filter books based on search query
   const filteredBooks = books.filter(book =>
