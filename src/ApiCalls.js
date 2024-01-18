a5d9956a0cf62c3e6eb674ca7ab837efb9bc1bdf
sapanajain9826 <sapanajain9826@gmail.com>
2024-01-18 07:25:33 +0530

    Updated the backend API_BASE_URL

--- src/ApiCalls.js
+++ src/ApiCalls.js
@@ -1,27 +1,27 @@
 // Assume you have an API endpoint for fetching issue history and issue requests
-const API_BASE_URL = 'https://your-api-base-url.com';
+const API_BASE_URL = 'http://localhost:8080/api';
 
-const fetchAllBooks = async () => {
-    try {
-      const response = await fetch(`/books/`);
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error('Error fetching books:', error);
-      return [];
-    }
-  };
+export const fetchAllBooks = async () => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/books/`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching books:', error);
+    return [];
+  }
+};
 
 const fetchBook = async (id) => {
-    try {
-      const response = await fetch(`/books/${id}`);
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error('Error fetching book details:', error);
-      return null;
-    }
-  };
+  try {
+    const response = await fetch(`/books/${id}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching book details:', error);
+    return null;
+  }
+};
 
 
 // Function to fetch all issue history from the API
@@ -29,59 +29,59 @@ const fetchIssueHistory = async () => {
   try {
     const response = await fetch(`${API_BASE_URL}/issues`);
     const data = await response.json();
-    return data; 
+    return data;
   } catch (error) {
     console.error('Error fetching issue history:', error);
-    return []; 
+    return [];
   }
 };
 
 const fetchIssueHistorySingleUser = async (userId) => {
-    try {
-      const response = await fetch(`${API_BASE_URL}/issues?userId=${userId}`);
-      const data = await response.json();
-      return data; 
-    } catch (error) {
-      console.error('Error fetching issue history:', error);
-      return []; 
-    }
-  };
+  try {
+    const response = await fetch(`${API_BASE_URL}/issues?userId=${userId}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching issue history:', error);
+    return [];
+  }
+};
 
 const fetchIssueHistorySingleBook = async (bookId) => {
-    try {
-      // Replace 'your-api-base-url' with the actual base URL of your API
-      const response = await fetch(`/issues?bookId=${bookId}`);
-      const data = await response.json();
-  
-      return data;
-    } catch (error) {
-      console.error('Error fetching issue history:', error);
-      return [];
-    }
+  try {
+    // Replace 'your-api-base-url' with the actual base URL of your API
+    const response = await fetch(`/issues?bookId=${bookId}`);
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error('Error fetching issue history:', error);
+    return [];
+  }
 };
-  
+
 // Function to fetch all issue requests from the API
 const fetchIssueRequests = async () => {
   try {
     const response = await fetch(`${API_BASE_URL}/requests`);
     const data = await response.json();
-    return data; 
+    return data;
   } catch (error) {
     console.error('Error fetching issue requests:', error);
-    return []; 
+    return [];
   }
 };
 
 // Function to fetch all issue requests from the API
 const fetchIssueRequestsSingleUser = async (userId) => {
-    try {
-      const response = await fetch(`${API_BASE_URL}/requests?userId=${userId}`);
-      const data = await response.json();
-      return data; 
-    } catch (error) {
-      console.error('Error fetching issue requests:', error);
-      return []; 
-    }
-  };
-  
+  try {
+    const response = await fetch(`${API_BASE_URL}/requests?userId=${userId}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching issue requests:', error);
+    return [];
+  }
+};
+
 
